9fa0242a0218ad1a16e9990d0953a9a0a46b8793
19alema <alemaedrick85@gmail.com>
2023-11-15 23:48:26 +0300

    updated commit

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const morgan = require('morgan')
 const phoneRouter = require('./routes/routes');
+const cors = require('cors')
 
 const {homePage} = require('./controller/controller')
 
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/info', homePage)
 app.use('/api/persons', phoneRouter)
 app.use(morgan('tiny'))
-
+app.use(express.static('dist'))
 
 
 const PORT = process.env.PORT || 5000
