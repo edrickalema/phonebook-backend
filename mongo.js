e3772b933aad7abf0fc548b0e0a8bce93eaf6e8e
19alema <alemaedrick85@gmail.com>
2023-11-20 14:05:42 +0300

    cleared console

--- mongo.js
+++ mongo.js
@@ -4,7 +4,7 @@ const app = require('./app')
 dotenv.config({path:'./config.env'})
 
 const DB = process.env.DATABASE_URI.replace('<PASSWORD>', process.env.DB_PASSWORD)
-console.log(DB)
+
 mongoose.set('strictQuery', false)
 
 mongoose.connect(DB).then(res=>console.log('Connected to DB')).catch((err) => console.log(err.message))
